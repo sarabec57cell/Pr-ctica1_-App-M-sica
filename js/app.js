35e02a67380c6c79cb7e9cca473f7d873a92ba8c
sarabec57cell <sarabece57@gmail.com>
2026-03-24 20:30:37 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
+function mostrarSeccion(seccionId) {
+    const secciones = document.querySelectorAll('.seccion');
 
+    secciones.forEach(seccion => {
+        seccion.classList.remove('activa');
+    });
+
+    document.getElementById(seccionId).classList.add('activa');
+}
